5472d214f6ecc029a3984519c8a212d0c20bd9e4
Burabari <burabaridev@gmail.com>
2023-01-22 12:05:54 +0100

    create playground page

---
+++ pages/playground.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const Playground: NextPage = () => {
+  return(
+    <div>
+      <div></div>
+      <div>This is the playground</div>
+    </div>
+  );
+}
+
+export default Playground;
