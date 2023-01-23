9dde6413dd53196049aef58f05b9895eabb551a1
Burabari <burabaridev@gmail.com>
2023-01-23 16:33:50 +0100

    add the playground page

--- pages/playground.tsx
+++ pages/playground.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next';
+import styles from '../styles/Playground.module.css';
 
 const Playground: NextPage = () => {
-  return(
-    <div>
-      <div></div>
-      <div>This is the playground</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.topBar}>Top-Bar content comes here</div>
+      <div className={styles.main}>
+        <div className={styles.sideBar}></div>
+        <div className={styles.content}>This is the playground</div>
+      </div>
     </div>
   );
 }
